57d26b21c76359a6543a7b01084b5498d0d69e30
nholvt <nholvt@vng.com.vn>
2019-08-16 16:21:39 +0700

    16/8/2019

--- src/main/scala/com/udemy/akka/essentials/part3Testing/InterceptingLogsSpec.scala
+++ src/main/scala/com/udemy/akka/essentials/part3Testing/InterceptingLogsSpec.scala
@@ -1,10 +1,11 @@
 package com.udemy.akka.essentials.part3Testing
 
-import akka.actor.{Actor, ActorSystem, Props}
-import akka.testkit.{ImplicitSender, TestKit}
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.testkit.{EventFilter, ImplicitSender, TestKit}
+import com.typesafe.config.ConfigFactory
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
-class InterceptingLogsSpec extends TestKit(ActorSystem("InterceptingLogsSpec"))
+class InterceptingLogsSpec extends TestKit(ActorSystem("InterceptingLogsSpec", ConfigFactory.load().getConfig("interceptingLogMessages")))
   with ImplicitSender
   with WordSpecLike
   with BeforeAndAfterAll{
@@ -13,6 +14,27 @@ class InterceptingLogsSpec extends TestKit(ActorSystem("InterceptingLogsSpec"))
     TestKit.shutdownActorSystem(system)
   }
 
+  import InterceptingLogsSpec._
+
+  "A checkout flow" should {
+    "correctly log the dispatch of an order" in {
+
+      val item = "Rock the JVM Akka course"
+      val creditCard = "1234-1234-1234-1234"
+
+      // Trong nhiều trường hợp chúng ta không thể can thiệp sâu và actor hierarchy để test
+      // Khi đó chúng ta có thể test dựa vào những logs mà các actor ghi ra
+
+      // EventFilter to filter logs
+      // Để EventFilter có thể catch logs chúng ta cần set akka.testkit.TestEventListener loger cho The ActorSystem of this testkit
+      // To use re-gex to filter log messages, we need to pass in a Pattern
+      EventFilter.info(pattern = s"Order [0-9]+ for item  $item has been dispatched", occurrences = 1) intercept {
+        // our test code
+        val checkoutRef = system.actorOf(Props[CheckoutActor])
+        checkoutRef ! Checkout(item, creditCard)
+      }
+    }
+  }
 }
 
 object InterceptingLogsSpec {
@@ -51,12 +73,14 @@ object InterceptingLogsSpec {
     }
   }
 
-  class FulfilmentManeger extends Actor {
+  class FulfilmentManeger extends Actor with ActorLogging{
     // Ở đây ta có thể dùng context.become để chuyển đổi ngữ cảnh thay vì dùng variable
     var orderId = 43
     override def receive: Receive = {
       case DispatchOrder(item: String) =>
         orderId += 1
+
+        log.info(s"Order $orderId for item $item has been dispatched")
         sender ! OrderConfirmed
     }
   }
