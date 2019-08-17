ba48959bc50b9cb0c2de61ee1155f91860b48137
tinhot97@gmail.com <tinhot97@gmail.com>
2019-08-17 19:26:23 +0700

    Begin part 5 of Akka essential course

--- src/main/scala/com/udemy/akka/essentials/part4FaultTolerance/SupervisionSpec.scala
+++ src/main/scala/com/udemy/akka/essentials/part4FaultTolerance/SupervisionSpec.scala
@@ -1,7 +1,8 @@
 package com.udemy.akka.essentials.part4FaultTolerance
 
-import akka.actor.{Actor, ActorSystem}
-import akka.testkit.{ImplicitSender, TestKit}
+import akka.actor.SupervisorStrategy.{Escalate, Restart, Resume, Stop}
+import akka.actor.{Actor, ActorRef, ActorSystem, AllForOneStrategy, OneForOneStrategy, Props, SupervisorStrategy, Terminated}
+import akka.testkit.{EventFilter, ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
 class SupervisionSpec extends TestKit(ActorSystem("SupervisionSpec"))
@@ -12,12 +13,167 @@ class SupervisionSpec extends TestKit(ActorSystem("SupervisionSpec"))
     TestKit.shutdownActorSystem(system)
   }
 
+  import SupervisionSpec._
 
+  "A supervisor" should {
+    "resume its child in case of a minor fault" in {
+      val supervisor = system.actorOf(Props[Supervisor])
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      child ! "I love akka"
+      child ! Report
+      expectMsg(3)
+
+      child ! "Akka is awesome because I am learning to think in a whole new way"
+      child ! Report
+      expectMsg(3)
+    }
+
+    "restart its child in case of an empty sentence" in {
+      val supervisor = system.actorOf(Props[Supervisor])
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+      child ! "I love akka"
+      child ! Report
+      expectMsg(3)
+
+      child ! ""
+      child ! Report
+      expectMsg(0)
+    }
+
+    "terminate its child in case of a major error" in {
+      val supervisor = system.actorOf(Props[Supervisor])
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      /*
+       When TestActor watch the child actor,
+       the child actor will give a terminated messages when it terminates
+       */
+      watch(child)
+      child ! "akka is nice"
+      val terminatedMessage = expectMsgType[Terminated]
+      assert(terminatedMessage.actor == child)
+    }
+
+    "escalate an error when it doesn't know what to do" in {
+      val supervisor = system.actorOf(Props[Supervisor],"supervisor")
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      // Make TestActor watch the child actor
+      watch(child)
+      child ! 43
+      val terminatedMessage = expectMsgType[Terminated]
+      assert(terminatedMessage.actor == child)
+    }
+  }
+
+  "A kinder supervisor" should {
+    "not kill children in case it's restarted or escalates failures" in {
+      val supervisor = system.actorOf(Props[NoDeathOnRestartSupervisor] ,"supervisor")
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      child ! "Akka is cool"
+      child ! Report
+      expectMsg(3)
+      child ! 45
+      child ! Report
+      expectMsg(0)
+    }
+  }
+
+  /*
+  A test suit to demo the AllForOneStrategy
+   */
+  "An all-for-one supervisor" should {
+    "apply the all-for-one strategy" in {
+      val supervisor = system.actorOf(Props[AllForOneSupervisor], "allForOneSupervisor")
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      supervisor ! Props[FussyWordCounter]
+      val secondChild = expectMsgType[ActorRef]
+
+      EventFilter[NullPointerException]() intercept {
+        child ! ""
+      }
+
+      secondChild ! Report
+      expectMsg(0)
+    }
+  }
 }
 
 object SupervisionSpec {
 
-  class FussyWordCounter extends Actor {
+  /*
+  A Supervisor Actor have a FussyWordCounter actor as its child.
+   */
+  class Supervisor extends Actor {
+    override val supervisorStrategy: SupervisorStrategy = OneForOneStrategy() {
+      case _: NullPointerException => Restart
+      case _: IllegalArgumentException => Stop
+      case _: RuntimeException => Resume
+        /*
+        Escalate means the Supervisor will stop its child and escalate the exception to its parent,
+        So the exception will reach the Supervisor
+         */
+      case _: Exception => Escalate
+    }
 
+    override def receive: Receive = {
+      case props: Props =>
+        val childRef = context.actorOf(props)
+        sender ! childRef
+    }
+  }
+
+  /*
+  By default, the actor will terminates/stops it childs when it restarts,
+  so to avoid stoping its childs, we override the preRestart method.
+   */
+  class NoDeathOnRestartSupervisor extends Supervisor {
+    override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+      // empty
+    }
+  }
+
+
+  /*
+  AllForOneStrategy means apply the supervisor strategy to all the children
+   */
+  class AllForOneSupervisor extends Supervisor {
+    override val supervisorStrategy = AllForOneStrategy() {
+      case _: NullPointerException => Restart
+      case _: IllegalArgumentException => Stop
+      case _: RuntimeException => Resume
+      /*
+      Escalate means the Supervisor will stop its child and escalate the exception to its parent,
+      So the exception will reach the Supervisor
+       */
+      case _: Exception => Escalate
+    }
+  }
+
+  /*
+  FussyWordCounter Actor đếm và tổng hợp lại số lượng từ trong mỗi câu,
+  được gửi đến
+   */
+  case object Report
+  class FussyWordCounter extends Actor {
+    var words: Int = 0
+    override def receive: Receive = {
+      case Report => sender() ! words
+      case "" => throw new NullPointerException("sentence is empty")
+      case sentence: String =>
+        if (sentence.length > 20) throw new RuntimeException("sentence is too big")
+        else if(!Character.isUpperCase(sentence(0))) throw new IllegalArgumentException("sentence must start with uppercase character")
+        else words += sentence.split(" ").length
+      case _ => throw new Exception("con only receive strings")
+    }
   }
 }
---
+++ src/main/scala/com/udemy/akka/essentials/part5Infrastructure/TimersSchedulers.scala
@@ -0,0 +1,5 @@
+package com.udemy.akka.essentials.part5Infrastructure
+
+object TimersSchedulers {
+
+}
